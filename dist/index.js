bc5f26f7a122efd9970a0f674363e3fe2876e775
Na Lin <lna483018@gmail.com>
2023-03-17 01:12:22 -0400

    create todo array to store todo items

--- dist/index.js
+++ dist/index.js
@@ -3,15 +3,24 @@ const todoInput = document.getElementById("todoInput");
 const submitButton = document.getElementById("btn");
 const form = document.getElementById("addTodoForm");
 const todoList = document.getElementById("todoList");
+const todos = [];
 function handleAddTodo(e) {
     e.preventDefault();
+    const newTodo = {
+        text: todoInput.value,
+        completed: false,
+    };
+    todos.push(newTodo);
+    createTodoElement(newTodo);
+    todoInput.value = "";
+}
+function createTodoElement(todo) {
     const newTodo = document.createElement("li");
-    newTodo.textContent = todoInput.value;
+    newTodo.textContent = todo.text;
     // add checkbox to each todo
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     newTodo.append(checkbox);
     todoList === null || todoList === void 0 ? void 0 : todoList.append(newTodo);
-    todoInput.value = "";
 }
 form === null || form === void 0 ? void 0 : form.addEventListener("submit", handleAddTodo);
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,28 @@ const submitButton = document.getElementById("btn");
 const form = document.getElementById("addTodoForm");
 const todoList = document.getElementById("todoList");
 
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+const todos: Todo[] = [];
+
 function handleAddTodo(e: SubmitEvent) {
   e.preventDefault();
 
+  const newTodo: Todo = {
+    text: todoInput.value,
+    completed: false,
+  };
+  todos.push(newTodo);
+  createTodoElement(newTodo);
+
+  todoInput.value = "";
+}
+
+function createTodoElement(todo: Todo) {
   const newTodo = document.createElement("li");
-  newTodo.textContent = todoInput.value;
+  newTodo.textContent = todo.text;
 
   // add checkbox to each todo
   const checkbox = document.createElement("input");
@@ -15,8 +32,6 @@ function handleAddTodo(e: SubmitEvent) {
   newTodo.append(checkbox);
 
   todoList?.append(newTodo);
-
-  todoInput.value = "";
 }
 
 form?.addEventListener("submit", handleAddTodo);
