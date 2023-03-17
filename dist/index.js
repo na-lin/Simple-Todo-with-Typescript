506df1e466bb94ec86c4748ac7eee2ccb6e67112
Na Lin <lna483018@gmail.com>
2023-03-17 01:06:09 -0400

    add event listener to form

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log("hello world");
+const todoInput = document.getElementById("todoInput");
+const submitButton = document.getElementById("btn");
+const form = document.getElementById("addTodoForm");
+const todoList = document.getElementById("todoList");
+function handleAddTodo(e) {
+    e.preventDefault();
+    console.log("submit");
+}
+form === null || form === void 0 ? void 0 : form.addEventListener("submit", handleAddTodo);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world");
+const todoInput = document.getElementById("todoInput");
+const submitButton = document.getElementById("btn");
+const form = document.getElementById("addTodoForm");
+const todoList = document.getElementById("todoList");
+
+function handleAddTodo(e: SubmitEvent) {
+  e.preventDefault();
+  console.log("submit");
+}
+
+form?.addEventListener("submit", handleAddTodo);
