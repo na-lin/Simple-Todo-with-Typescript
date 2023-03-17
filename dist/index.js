f70dff665173f2a1e0e95e2b152ed4a346e5c36a
Na Lin <lna483018@gmail.com>
2023-03-17 01:07:52 -0400

    append li to ul after each form submit

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,9 @@ const form = document.getElementById("addTodoForm");
 const todoList = document.getElementById("todoList");
 function handleAddTodo(e) {
     e.preventDefault();
-    console.log("submit");
+    const newTodo = document.createElement("li");
+    newTodo.textContent = todoInput.value;
+    todoList === null || todoList === void 0 ? void 0 : todoList.append(newTodo);
+    todoInput.value = "";
 }
 form === null || form === void 0 ? void 0 : form.addEventListener("submit", handleAddTodo);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
-const todoInput = document.getElementById("todoInput");
+const todoInput = document.getElementById("todoInput") as HTMLInputElement;
 const submitButton = document.getElementById("btn");
 const form = document.getElementById("addTodoForm");
 const todoList = document.getElementById("todoList");
 
 function handleAddTodo(e: SubmitEvent) {
   e.preventDefault();
-  console.log("submit");
+
+  const newTodo = document.createElement("li");
+  newTodo.textContent = todoInput.value;
+  todoList?.append(newTodo);
+
+  todoInput.value = "";
 }
 
 form?.addEventListener("submit", handleAddTodo);
